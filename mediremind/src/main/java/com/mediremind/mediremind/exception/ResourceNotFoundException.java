8bc08160282f189414557db18ad389b9c1ea3d35
Shrikant Shinde <110349806+shrikantshinde178@users.noreply.github.com>
2024-06-09 11:52:38 +0530

    Delete mediremind directory

--- mediremind/src/main/java/com/mediremind/mediremind/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.mediremind.mediremind.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
